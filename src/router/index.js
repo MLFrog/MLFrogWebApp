6f37ce27254af66b90f3725dd858c9246fabdfba
모모준 <jmjm9797@naver.com>
2023-05-05 16:30:14 +0900

    라우터설정 추가

--- src/router/index.js
+++ src/router/index.js
@@ -46,6 +46,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/JoinRoom.vue')
+  },
+  {
+    path: '/room',
+    name: 'room',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/RoomDetail.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminBoard.vue')
   }
 ]
 
